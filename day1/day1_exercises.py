1a9d93b5b15ba9fd67f6802042aab3e8dcbf0ec3
x00143663 <x00143663@gmail.com>
2025-11-26 14:20:55 +0000

    Add Day 1 - Exercise 1 (variables and types)

---
+++ day1/day1_exercises.py
@@ -0,0 +1,10 @@
+# day1_exercises.py
+# Ziua 1 - Exercițiul 1: variabile și tipuri de date
+
+nume = "Nicolae"
+varsta = 30          # poți schimba cu vârsta ta reală :)
+oras = "Naas"
+este_student = False
+
+print(f"Numele meu este {nume}, am {varsta} ani, locuiesc în {oras} și este_student={este_student}.")
+
